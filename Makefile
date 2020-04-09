3514fc17f894a457f9297acbb964f9ad8e173e48
Stefan Yip <stefanyip@hotmail.com>
2020-04-09 11:30:28 +0200

    refactor files and makefile

--- Makefile
+++ Makefile
@@ -1,15 +1,32 @@
-make_chip8:
-	mkdir -p build/; \
-	gcc -o build/chip8 chip8.c -lSDL2
+CC = gcc
+CFLAGS = -Werror
+LDFLAGS = -lSDL2
+OBJFILES = chip8.o
+TARGET = chip8
+BUILD_PATH = ./build
+BIN_PATH = ./bin
+SRC_PATH = ./src
+
+all: $(TARGET)
+
+$(TARGET): $(OBJFILES)
+	mkdir -p $(BUILD_PATH); \
+	$(CC) $(CFLAGS) -o $(BUILD_PATH)/$(TARGET) $(SRC_PATH)/$(OBJFILES) $(LDFLAGS)
+
+chip8.o: $(SRC_PATH)/chip8.c $(SRC_PATH)/chip8.h
+	cd $(SRC_PATH); \
+	$(CC) $(CFLAGS) -c chip8.c
 
 run:
-	build/chip8
+	./build/chip8
 
 dl_roms:
 	mkdir -p roms/ tmp/; \
 	cd tmp/; \
 	git clone https://github.com/stianeklund/chip8.git; \
-	cp chip8/roms/*.ch8 ../roms
+	cp chip8/roms/*.ch8 ../roms; \
+	cd ..; \
+	rm -rf ./tmp
 
 clean:
-	rm -rf roms/ build/
+	rm -rf roms/ build/ src/*.o
---
+++ src/chip8.c
@@ -0,0 +1,339 @@
+#include "chip8.h"
+#include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+chip8_t *init_chip8()
+{
+	chip8_t *chip8 = malloc(sizeof(chip8_t));
+
+	chip8->pc = 0x200;
+	chip8->I = 0;
+	chip8->sp = 0;
+	chip8->t_delay = 0;
+	chip8->t_sound = 0;
+	memset(chip8->mem, 0, sizeof(chip8->mem));
+	memset(chip8->V, 0, sizeof(chip8->V));
+	memset(chip8->frame_buf, 0, sizeof(chip8->frame_buf));
+	memset(chip8->stack, 0, sizeof(chip8->stack));
+
+	memcpy(chip8->mem, chip8_fontset, sizeof(chip8_fontset));
+
+	printf("CHIP-8 initialized\n");
+
+	return chip8;
+}
+
+int load_program(chip8_t *chip8)
+{
+	char *file_path = "roms/WALL.ch8";
+	FILE *f = fopen(file_path, "rb");
+
+	struct stat s;
+	stat(file_path, &s);
+	size_t ret = fread(chip8->mem + 0x200, 1, s.st_size, f);
+
+	if (ret != s.st_size)
+	{
+		printf("Program loaded unsuccesfully\n");
+		return -1;
+	}
+	printf("Program loaded succesfully\n");
+	return 1;
+}
+
+int init_gfx()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	window = SDL_CreateWindow("Chip-8 Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	if (window == NULL)
+	{
+		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == NULL)
+	{
+		printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	return 1;
+}
+
+int close_gfx()
+{
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+int stop()
+{
+	close_gfx();
+}
+
+int init()
+{
+	init_gfx();
+
+	return 0;
+}
+
+void draw_screen(chip8_t *chip8)
+{
+	for (int i = 0; i < SCREEN_HEIGHT; i++)
+	{
+		for (int j = 0; j < SCREEN_WIDTH; j++)
+		{
+			if (chip8->frame_buf[j][i] == 1)
+				SDL_RenderDrawPoint(renderer, j, i);
+		}
+	}
+	SDL_RenderPresent(renderer);
+	chip8->draw_flag = 0;
+}
+
+uint8_t spr_addr(uint8_t c)
+{
+	if (c > 0xF)
+		return 0;
+
+	return c;
+}
+
+void inc_pc(chip8_t *chip8)
+{
+	chip8->pc += 2;
+}
+
+void emulate_cycle(chip8_t *chip8)
+{
+	// Fetch Opcode
+	uint16_t opcode = chip8->mem[chip8->pc] << 8 | chip8->mem[chip8->pc + 1];
+	printf("pc: %04x\topcode: %04x\tI: %04x\n", chip8->pc, opcode, chip8->I);
+	// Decode Opcode
+	uint16_t a, b, c, d = 0;
+	switch (opcode & 0xF000)
+	{
+	case 0x0000:
+		switch (opcode & 0x00FF)
+		{
+		case 0x00E0:
+			memset(chip8->frame_buf, 0, sizeof(chip8->frame_buf));
+			chip8->draw_flag = 1;
+			inc_pc(chip8);
+			break;
+		case 0x00EE:
+			chip8->sp--;
+			chip8->pc = chip8->stack[chip8->sp];
+			break;
+		default:
+			// Calls RCA 1802 program at address 0x0NNN. Not necessary for most ROMs.
+			break;
+		}
+		break;
+	case 0x1000:
+		chip8->pc = opcode & 0x0FFF;
+		break;
+	case 0x2000:
+		chip8->stack[chip8->sp] = chip8->pc;
+		chip8->sp++;
+		chip8->pc = opcode & 0x0FFF;
+		break;
+	case 0x3000:
+		if (chip8->V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+			inc_pc(chip8);
+		inc_pc(chip8);
+		break;
+	case 0x4000:
+		if (chip8->V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+			inc_pc(chip8);
+		inc_pc(chip8);
+		break;
+	case 0x5000:
+		if (chip8->V[(opcode & 0x0F00) >> 8] == chip8->V[(opcode & 0x00F0) >> 4])
+			inc_pc(chip8);
+		inc_pc(chip8);
+		break;
+	case 0x6000:
+		chip8->V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+		inc_pc(chip8);
+		break;
+	case 0x7000:
+		chip8->V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+		inc_pc(chip8);
+		break;
+	case 0x8000:
+		switch (opcode & 0x000F)
+		{
+		case 0x0000:
+			chip8->V[(opcode & 0x0F00) >> 8] = chip8->V[(opcode & 0x00F0) >> 4];
+			break;
+		case 0x0001:
+			chip8->V[(opcode & 0x0F00) >> 8] |= chip8->V[(opcode & 0x00F0) >> 4];
+			break;
+		case 0x0002:
+			chip8->V[(opcode & 0x0F00) >> 8] &= chip8->V[(opcode & 0x00F0) >> 4];
+			break;
+		case 0x0003:
+			chip8->V[(opcode & 0x0F00) >> 8] ^= chip8->V[(opcode & 0x00F0) >> 4];
+			break;
+		case 0x0004:
+			chip8->V[0xF] = chip8->V[(opcode & 0x0F00) >> 8] + chip8->V[(opcode & 0x00F0) >> 4] > 0xFF ? 1 : 0;
+			chip8->V[(opcode & 0x0F00) >> 8] += chip8->V[(opcode & 0x00F0) >> 4];
+			break;
+		case 0x0005:
+			chip8->V[0xF] = chip8->V[(opcode & 0x0F00) >> 8] > chip8->V[(opcode & 0x00F0) >> 4] ? 1 : 0;
+			chip8->V[(opcode & 0x0F00) >> 8] = chip8->V[(opcode & 0x0F00) >> 8] - chip8->V[(opcode & 0x00F0) >> 4];
+			break;
+		case 0x0006:
+			chip8->V[0xF] = chip8->V[(opcode & 0x0F00) >> 8] & 1 == 1 ? 1 : 0;
+			chip8->V[(opcode & 0x0F00) >> 8] >>= 1;
+			break;
+		case 0x0007:
+			chip8->V[0xF] = chip8->V[(opcode & 0x00F0) >> 4] > chip8->V[(opcode & 0x0F00) >> 8] ? 1 : 0;
+			chip8->V[(opcode & 0x00F0) >> 4] = chip8->V[(opcode & 0x00F0) >> 4] - chip8->V[(opcode & 0x0F00) >> 8];
+			break;
+		case 0x000E:
+			chip8->V[0xF] = chip8->V[(opcode & 0x0F00) >> 8] & 1 == 1 ? 1 : 0;
+			chip8->V[(opcode & 0x0F00) >> 8] <<= 1;
+			break;
+		default:
+			break;
+		}
+		inc_pc(chip8);
+		break;
+	case 0x9000:
+		if (chip8->V[(opcode & 0x0F00) >> 8] != chip8->V[(opcode & 0x00F0) >> 4])
+			inc_pc(chip8);
+		inc_pc(chip8);
+		break;
+	case 0xA000:
+		chip8->I = opcode & 0x0FFF;
+		inc_pc(chip8);
+		break;
+	case 0xB000:
+		chip8->pc = (opcode & 0x0FFF) + chip8->V[0];
+		break;
+	case 0xC000:
+		chip8->V[(opcode & 0x0F00) >> 8] = (rand() % 256) & (opcode & 0x00FF);
+		inc_pc(chip8);
+		break;
+	case 0xD000:
+		a = chip8->V[(opcode & 0x0F00) >> 8]; // x
+		b = chip8->V[(opcode & 0x00F0) >> 4]; // y
+		c = opcode & 0x000F;				  // amount
+		// printf("drawing x: %d, y: %d, n: %d\n", a, b, c);
+		chip8->V[0xF] = 0;
+		for (int yline = 0; yline < c; yline++)
+		{
+			d = chip8->mem[chip8->I + yline];
+			for (int xline = 0; xline < 8; xline++)
+			{
+				if ((d & (0x80 >> xline)) != 0)
+				{
+					if (chip8->frame_buf[a + xline][b + yline] == 1)
+						chip8->V[0xF] = 1;
+					chip8->frame_buf[a + xline][b + yline] ^= 1;
+				}
+			}
+		}
+
+		chip8->draw_flag = 1;
+		inc_pc(chip8);
+		break;
+	case 0xE000:
+		switch (opcode & 0x00FF)
+		{
+		case 0x009E:
+			/* code */
+			break;
+		case 0x00A1:
+			/* code */
+			break;
+		default:
+			break;
+		}
+		inc_pc(chip8);
+		break;
+	case 0xF000:
+		switch (opcode & 0x00FF)
+		{
+		case 0x0007:
+			chip8->V[(opcode & 0x0F00) >> 8] = chip8->t_delay;
+			break;
+		case 0x000A:
+			chip8->key_press = 0;
+			while (!chip8->key_press)
+				;
+			chip8->V[(opcode & 0x0F00) >> 8] = chip8->key_press;
+			break;
+		case 0x0015:
+			chip8->t_delay = chip8->V[(opcode & 0x0F00) >> 8];
+			break;
+		case 0x0018:
+			chip8->t_sound = chip8->V[(opcode & 0x0F00) >> 8];
+			break;
+		case 0x001E:
+			chip8->I += (opcode & 0x0F00) >> 8;
+			break;
+		case 0x0029:
+			chip8->I = spr_addr(chip8->V[(opcode & 0x0F00) >> 8]);
+			break;
+		case 0x0033:
+			a = (opcode & 0x0F00) >> 8;
+			chip8->mem[chip8->I] = chip8->V[a] / 100;
+			chip8->mem[chip8->I + 1] = chip8->V[a] % 100 / 10;
+			chip8->mem[chip8->I + 2] = chip8->V[a] % 100 % 10;
+			break;
+		case 0x0055:
+			memcpy(chip8->mem + chip8->I, chip8->V, (opcode & 0x0F00) >> 8);
+			break;
+		case 0x0065:
+			memcpy(chip8->V, chip8->mem + chip8->I, (opcode & 0x0F00) >> 8);
+			break;
+		default:
+			break;
+		}
+		inc_pc(chip8);
+		break;
+	}
+
+	if (chip8->t_delay > 0)
+	{
+		chip8->t_delay--;
+	}
+	if (chip8->t_sound > 0)
+	{
+		chip8->t_sound--;
+	}
+}
+
+int main()
+{
+	chip8_t *c = init_chip8();
+	init_gfx();
+	load_program(c);
+
+	while (1)
+	{
+		emulate_cycle(c);
+		if (c->draw_flag)
+			draw_screen(c);
+		usleep(5000);
+		// usleep(500000);
+	}
+
+	return 0;
+}
